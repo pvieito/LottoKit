c8f06ff37b4361d8e19a5e13339278bb0850f28d
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-03-25 01:31:48 +0100

    Swift 5 + macOS Swift PM support

--- Package.swift
+++ Package.swift
@@ -1,19 +1,33 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "LottoKit",
     products: [
-        .executable(name: "LottoTool", targets: ["LottoTool"]),
-        .library(name: "LottoKit", targets: ["LottoKit"])
+        .executable(
+            name: "LottoTool",
+            targets: ["LottoTool"]
+        ),
+        .library(
+            name: "LottoKit",
+            targets: ["LottoKit"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/pvieito/CommandLineKit.git", .branch("master")),
         .package(url: "https://github.com/pvieito/LoggerKit.git", .branch("master")),
     ],
     targets: [
-        .target(name: "LottoTool", dependencies: ["LottoKit", "CommandLineKit"], path: "LottoTool"),
-        .target(name: "LottoKit", dependencies: ["LoggerKit"], path: "LottoKit")
+        .target(
+            name: "LottoTool",
+            dependencies: ["LottoKit", "CommandLineKit"],
+            path: "LottoTool"
+        ),
+        .target(
+            name: "LottoKit",
+            dependencies: ["LoggerKit"],
+            path: "LottoKit"
+        )
     ]
 )
