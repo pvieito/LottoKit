d241aede06efc0ec5d49bda9dc342e0e3d3f2d9c
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-07-02 19:58:05 +0200

    Simpler implementation

--- LottoKit/LottoManager.swift
+++ LottoKit/LottoManager.swift
@@ -59,43 +59,10 @@ public struct LottoManager {
     
     public static func checkLotto(number: Int, mode: LottoMode? = nil) throws -> LottoResult {
         let mode = mode ?? LottoMode.defaultMode
+        let url = self.getLottoURL(number: number, mode: mode)
+        let responseString = try String(contentsOf: url)
         
-        var data: Data?
-        var response: URLResponse?
-        var error: Error?
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        let taskQueue = DispatchQueue(label: "com.pvieito.LottoTool.URLSession")
-        
-        taskQueue.async {
-            let dataTask = URLSession.shared.dataTask(with: self.getLottoURL(number: number, mode: mode)) { (taskData, taskResponse, taskError) in
-                (data, response, error) = (taskData, taskResponse, taskError)
-                semaphore.signal()
-            }
-            
-            dataTask.resume()
-        }
-        
-        semaphore.wait()
-        
-        if let error = error {
-            throw error
-        }
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw LottoError.serverError("Response not available")
-        }
-        
-        guard (200...299).contains(httpResponse.statusCode) else {
-            throw LottoError.httpError(httpResponse.statusCode)
-        }
-        
-        guard let responseData = data else {
-            throw LottoError.dataError("No data available")
-        }
-        
-        guard let responseString = String(bytes: responseData, encoding: .utf8),
-            responseString.hasPrefix(LottoManager.lottoApiResponsePreffix),
+        guard responseString.hasPrefix(LottoManager.lottoApiResponsePreffix),
             let jsonData = responseString.replacingOccurrences(of: LottoManager.lottoApiResponsePreffix, with: "").data(using: .utf8) else {
                 throw LottoError.dataError("Malformed response string")
         }
